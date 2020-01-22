2c866280d26c5ef5fa358326364401a2c696a9e1
Liseth20 <liseth341@gmail.com>
2020-01-22 14:33:05 -0500

    agregando la nueva clase de modulos

--- NameSpace/Newspace.js
+++
@@ -1,8 +0,0 @@
-//Una clase donde todos sus metodos son estaticos
-
-if (Validaciones.validadTexto("Barry Allen")) {
-    console.log("El texto es valido");
-}
-else {
-    console.log("El texto no es valido");
-}
